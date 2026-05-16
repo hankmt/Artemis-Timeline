f66f093d5949eadfab75c96b62a73b946929e8e5
Michael Rice <9098856+xcalibur839@users.noreply.github.com>
2026-05-16 03:42:18 -0700

    Implement initial camera settings formatting

--- fetch-from-nasa.py
+++ fetch-from-nasa.py
@@ -55,13 +55,33 @@ def format_datetime(metadata: json):
     return formatted_datetime
 
 def format_camera_settings(metadata: json):
+    SETTINGS_SEPERATOR = "·"
     settings = ""
 
+    focal_length = ""
+    if "EXIF:FocalLength" in metadata and len(metadata['EXIF:FocalLength']) > len(focal_length):
+        focal_length = metadata['EXIF:FocalLength']
+    # ...
+
+    lens_info = ""
+    if "EXIF:LensInfo" in metadata and len(metadata['EXIF:LensInfo']) > len(lens_info):
+        lens_info = metadata['EXIF:LensInfo']
+    # ...
+
+    exposure_time = ""
+    if "EXIF:ExposureTime" in metadata and len(str(metadata['EXIF:ExposureTime'])) > len(exposure_time):
+        exposure_time = f"{metadata['EXIF:ExposureTime']}s"
+    # ...
+
+    iso = ""
+    if "EXIF:ISO" in metadata and len(str(metadata['EXIF:ISO'])) > len(iso):
+        iso = f"ISO {metadata['EXIF:ISO']}"
+    # ...
+
+    settings = f" {SETTINGS_SEPERATOR} ".join((focal_length, lens_info, exposure_time, iso))
     return settings
 
 def get_metadata(nasa_id: str, file: str, original: str):
-    SETTINGS_SEPERATOR = "·"
-
     metadata_request = session.get(f"{BASE_URL}/metadata/{nasa_id}", headers = headers)
     metadata_location = json.loads(metadata_request.text)['location']
     metadata_file = session.get(metadata_location, headers = headers)
@@ -75,7 +95,7 @@ def get_metadata(nasa_id: str, file: str, original: str):
         "photographer": metadata['EXIF:Artist'] if "EXIF:Artist" in metadata else metadata['AVAIL:Photographer'] if "AVAIL:Photographer" in metadata else "",
         "location": metadata['AVAIL:Location'],
         "camera": metadata['EXIF:Model'] if "EXIF:Model" in metadata else "",
-        "settings": format_camera_settings(metadata), #f"{metadata['EXIF:FocalLength']} {SETTINGS_SEPERATOR} {metadata['EXIF:LensInfo']} {SETTINGS_SEPERATOR} {metadata['EXIF:ExposureTime']}s {SETTINGS_SEPERATOR} ISO {metadata['EXIF:ISO']} {SETTINGS_SEPERATOR} {None}",
+        "settings": format_camera_settings(metadata),
         "spacecraft": True,
         "batch": 1,
         "title": metadata['AVAIL:Title'] if "AVAIL:Title" in metadata else "",
